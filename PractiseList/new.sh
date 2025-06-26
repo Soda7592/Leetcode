98023fc5316780303e7b344c61f6679cefba5eb5
yt <ythuang@iii.org.tw>
2025-06-26 16:54:37 +0800

    modifiy: Change #D, #A to # D, # A

--- PractiseList/new.sh
+++ PractiseList/new.sh
@@ -7,5 +7,5 @@ folder_name=$(echo "$folder_name" | sed -E 's/__+/_/g')
 mkdir -p "$folder_name"
 touch ./$folder_name/sol.cpp
 touch ./$folder_name/README.md
-echo "#Description" >> ./$folder_name/README.md
-echo "#Approach" >> ./$folder_name/README.md
+echo "# Description" >> ./$folder_name/README.md
+echo "# Approach" >> ./$folder_name/README.md
