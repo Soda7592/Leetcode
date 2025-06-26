6db9acb59f305850e847c2ac4eafd585d2cb8a35
yt <ythuang@iii.org.tw>
2025-06-26 17:03:55 +0800

    feat: Creating folder/problem more convenience.

---
+++ Top150/new.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+folder_name=$(echo "$@" | tr '.' '_')
+folder_name=$(echo "$folder_name" | tr ' ' '_') 
+folder_name=$(echo "$folder_name" | sed -E 's/__+/_/g')
+
+mkdir -p "$folder_name"
+touch ./$folder_name/sol.cpp
+touch ./$folder_name/README.md
+echo "# Description" >> ./$folder_name/README.md
+echo "# Approach" >> ./$folder_name/README.md
